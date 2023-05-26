a3a967733a667da1faad471e39206c73e7c9a1ad
ranss@rambler.ru <ranss@rambler.ru>
2023-05-26 17:23:53 +0300

    начальный коммит

---
+++ src/main/java/com/homework27/HomeWork27Application.java
@@ -0,0 +1,13 @@
+package com.homework27;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWork27Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeWork27Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/homework27/HomeWork27ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homework27;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWork27ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
